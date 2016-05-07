0bdf1bd8eebbdd6d70452c6213b155840491921d
Dan Jakob Ofer <ofer987@gmail.com>
2016-05-07 19:05:29 -0400

    wip

--- fileutils/move_file.go
+++ fileutils/move_file.go
@@ -1,4 +1,4 @@
-package rename
+package fileutils
 
 import (
 	"io/ioutil"
--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/ofer987/rename/fileutils"
+	"os"
+)
 
+func main() {
+	DisplayHello()
+
+	os.Stdout.WriteString("The source file is:")
+	fmt.Printf("%+v\n", os.Args[1])
+	src := os.Args[1]
+	dest := src
+	fmt.Scanf("%s", &dest)
+	fmt.Printf("the new file name is %+v\n", dest)
+
+	fileutils.MoveFile(src, dest)
+}
+
+func DisplayHello() {
+	fmt.Printf("Hello World\n")
 }
